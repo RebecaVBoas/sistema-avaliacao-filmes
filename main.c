fb33c57bacd1eabac0e591b48798905f13aee981
Rebeca Vilas Boas <rebecavilasboasdejesus1@gmail.com>
2025-11-09 13:05:58 -0300

    feat: adicionar funções para abrir arquivos de usuário, filme e avaliações

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct
     int avaliacao;
     char comentario[80];
 
-} avaliar;
+} Avaliar;
 
 typedef struct
 {
@@ -20,14 +20,14 @@ typedef struct
     int qtdAvalia;
     int qtdComent;
 
-} filmes;
+} Filmes;
 
 typedef struct
 {
     char nome[50];
     char senha[8];
 
-} usuarios;
+} Usuarios;
 
 void menuPrincipal()
 {
@@ -40,9 +40,59 @@ void menuPrincipal()
     printf("                            4 - Sair                           \n");
 }
 
+FILE *abrirFilme(){
+    FILE *filmes;
+ 
+    filmes =fopen("filmes.data", "a+b");
+ 
+    if(filmes==NULL){
+        printf("ERRO, NÃO FOI POSÍVEL ABRIR O ARQUIVO FILMES");
+        return NULL;
+    }
+    else{
+        printf("\nARQUIVO FILME ABERTO \n");
+        return filmes;
+    }
+}
+ 
+FILE *abrirUsuarios(){
+    FILE *usuarios;
+ 
+    usuarios =fopen("usuarios.data", "a+b");
+    if(usuarios==NULL){
+        printf("\n ERRO NÃO FOI POSSIVEL ABRIR O ARQUIVO USUARIOS.DATA \n");
+        return NULL;
+    }
+    else{
+        printf("\nARQUIVO USUARIOS.DATA ABERTO\n");
+        return usuarios;
+    }
+}
+ 
+ 
+FILE *abrirAvaliacoes(){
+    FILE* avaliacoes;
+    avaliacoes = fopen("avaliacoes.data","a+b");
+ 
+    if(avaliacoes == NULL){
+        printf("\nERRO. NÃO FOI POSSIVEL ABRIRO ARQUIVO AVALIACOES.DATA\n");
+        return NULL;
+    }
+    else{
+        printf("\nARQUIVO AVALIACOES.DATA ABERTO\n");
+        return avaliacoes;
+    }
+}
+ 
+
+
 int main()
 {
-    usuarios login;
+     
+    FILE *filme = abrirFilme();
+    FILE *usuarios = abrirUsuarios();
+    FILE *avaliacoes= abrirAvaliacoes();
+    Usuarios login;
     /*
         declarações das variaveis
     */
